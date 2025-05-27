4a7c221fe9e1b51965b79b0f3cfea3fade9611ef
Bogdan <robotosoid2000@gmail.com>
2025-05-27 14:43:09 +0300

    0-0

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,10 @@
 import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import { showLoadBtn } from './render-functions';
 
-let page = 1;
 const perPage = 15;
 
-export const resetPage = () => {
-  page = 1;
-};
-
-export const getImagesByQuery = async query => {
+export const getImagesByQuery = async (query, page = 1) => {
   try {
     const response = await axios.get('https://pixabay.com/api/', {
       params: {
@@ -24,25 +18,14 @@ export const getImagesByQuery = async query => {
       },
     });
 
-    const hits = response.data.hits;
-
-    if (hits.length === 0) {
-      iziToast.warning({
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        position: 'topRight',
-      });
-      return [];
-    }
-
-    if (page > 1) {
-      showLoadBtn();
-    }
-
-    page += 1;
-    return hits;
+    return {
+      hits: response.data.hits,
+      totalHits: response.data.totalHits,
+    };
   } catch (error) {
-    console.log('The server did not return any images:', error);
-    return [];
+    iziToast.error({
+      message: 'Failed to fetch images. Please try again later.',
+      position: 'topRight',
+    });
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { getImagesByQuery, resetPage } from './js/pixabay-api.js';
+import { getImagesByQuery } from './js/pixabay-api.js';
 import {
   createGallery,
   clearGallery,
@@ -13,6 +13,7 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 let currentQuery = '';
+let currentPage;
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
@@ -27,20 +28,49 @@ form.addEventListener('submit', async e => {
   }
 
   currentQuery = searchValue;
-  resetPage();
+  currentPage = 1;
+
   clearGallery();
   hideLoadBtn();
   showLoader();
 
   try {
-    const images = await getImagesByQuery(currentQuery);
-    createGallery(images);
-    if (images.length === 15) {
+    const { hits, totalHits } = await getImagesByQuery(
+      currentQuery,
+      currentPage
+    );
+
+    if (hits.length === 0) {
+      iziToast.info({
+        message: 'No images found for your query.',
+        position: 'topRight',
+      });
+      return;
+    }
+
+    createGallery(hits);
+    const imageEl = document.querySelector('.image-card');
+    if (imageEl) {
+      const rect = imageEl.getBoundingClientRect();
+      const scrollOffset = rect.height * 2;
+
+      window.scrollBy({
+        top: scrollOffset,
+        behavior: 'smooth',
+      });
+    }
+
+    if (document.querySelectorAll('.image-card').length < totalHits) {
       showLoadBtn();
+    } else {
+      iziToast.warning({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
     }
   } catch (error) {
     iziToast.error({
-      message: 'Something went wrong while fetching images!',
+      message: 'Failed to fetch images!',
       position: 'topRight',
     });
   } finally {
@@ -52,10 +82,31 @@ loadMoreBtn.addEventListener('click', async () => {
   showLoader();
 
   try {
-    const images = await getImagesByQuery(currentQuery);
-    createGallery(images);
-    if (images.length < 15) {
+    currentPage += 1;
+    const { hits, totalHits } = await getImagesByQuery(
+      currentQuery,
+      currentPage
+    );
+
+    createGallery(hits);
+    const imageEl = document.querySelector('.image-card');
+    if (imageEl) {
+      const rect = imageEl.getBoundingClientRect();
+      const scrollOffset = rect.height * 2;
+
+      window.scrollBy({
+        top: scrollOffset,
+        behavior: 'smooth',
+      });
+    }
+
+    const totalShown = document.querySelectorAll('.image-card').length;
+    if (totalShown >= totalHits) {
       hideLoadBtn();
+      iziToast.warning({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
     }
   } catch (error) {
     iziToast.error({
